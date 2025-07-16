d9e531c628dbbf0bc5fa4f9bf19342d0e400bbb6
vinnietito <vincentnjuguna69@gmail.com>
2025-07-16 19:43:53 +0300

    Commits

--- utils/word_to_image.py
+++ utils/word_to_image.py
@@ -1,19 +1,24 @@
 import os
+import pythoncom
 from docx2pdf import convert
 from pdf2image import convert_from_path
 
 def convert_docx_to_images(docx_path, output_folder):
-    # Convert DOCX to PDF
-    pdf_path = os.path.join(output_folder, "temp.pdf")
+    # Ensure COM is initialized
+    pythoncom.CoInitialize()
+
+    # Step 1: Convert DOCX to PDF
+    base_filename = os.path.splitext(os.path.basename(docx_path))[0]
+    pdf_path = os.path.join(output_folder, base_filename + '.pdf')
     convert(docx_path, pdf_path)
 
-    # Convert PDF to images
-    images = convert_from_path(pdf_path, poppler_path=r"C:\Program Files\Poppler\poppler-24.08.0\Library\bin")
+    # Step 2: Convert PDF to images
+    images = convert_from_path(pdf_path)
 
     image_paths = []
-    for i, img in enumerate(images):
-        image_path = os.path.join(output_folder, f"page_{i + 1}.png")
-        img.save(image_path, "PNG")
+    for i, image in enumerate(images):
+        image_path = os.path.join(output_folder, f'page_{i+1}.png')
+        image.save(image_path, 'PNG')
         image_paths.append(image_path)
 
     return image_paths
