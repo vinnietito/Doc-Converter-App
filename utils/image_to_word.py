ba7e39aac926ca201655a72e4ecdcecbd553e692
vinnietito <vincentnjuguna69@gmail.com>
2025-07-16 18:32:44 +0300

    Commits

--- utils/image_to_word.py
+++ utils/image_to_word.py
@@ -2,6 +2,9 @@ from PIL import Image
 import pytesseract
 from docx import Document
 
+# 👉 Set path to your actual tesseract.exe
+pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+
 def convert_image_to_word(image_path, output_path):
     text = pytesseract.image_to_string(Image.open(image_path))
     doc = Document()
